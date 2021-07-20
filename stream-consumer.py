bda15ea0edcdbab1087a0dcd0c203a2b72e9d24b
huyduong2792000 <bapzangbo2792000@gmail.com>
2021-07-20 16:00:38 +0700

    update stream

--- stream-consumer.py
+++ stream-consumer.py
@@ -2,7 +2,7 @@ import os
 import json
 
 from kafka import KafkaConsumer
-TOPIC = 'test-druid1'
+TOPIC = 'test-druid'
 consumer = KafkaConsumer(TOPIC)
 for msg in consumer:
     print (msg)
--- stream-producer.py
+++ stream-producer.py
@@ -3,7 +3,7 @@ import json
 import time
 from kafka import KafkaProducer
 
-TOPIC = 'test-druid1'
+TOPIC = 'test-druid'
 BASE_PATH = "/data/2021-03-21/data/2021-03-21"
 def get_log_files():
     result = os.listdir(os.path.expanduser(BASE_PATH))
