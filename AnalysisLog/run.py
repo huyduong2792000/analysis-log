1147d0344250cab546e53237a71b59b8204c9dc3
huyduong2792000 <bapzangbo2792000@gmail.com>
2021-07-22 10:50:16 +0700

    update read nginx log

--- AnalysisLog/run.py
+++ AnalysisLog/run.py
@@ -43,6 +43,7 @@ def stream(raw_log, file_name):
     dict_log = parse_log_to_dict(raw_log)
     dict_log['file_name'] = file_name
     if dict_log:
+        print("===dict_log===", dict_log)
         json_producer.send(TOPIC, dict_log)
         json_producer.flush(30)
 
