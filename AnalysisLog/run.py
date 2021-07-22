affbfab25318d47ee006e0fdb540f757ad48dd81
huyduong2792000 <bapzangbo2792000@gmail.com>
2021-07-22 15:08:43 +0700

    update read nginx log

--- AnalysisLog/run.py
+++ AnalysisLog/run.py
@@ -1,68 +1,58 @@
 import re
-# import os
-import json
+from multiprocessing import Process, Manager
 import time
-from kafka import KafkaProducer
-from manager import Manager
+import itertools 
 
-from os import listdir
-from os.path import isfile, join
-# onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-
-manager = Manager()
-
-json_producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v, indent = 4).encode('utf-8'))
-log_dir = '/home/vunm/analysis-log'
-TOPIC = 'test-druid5'
-
-list_cofig = [
-    '$remote_addr $http_x_forwarded_for [$time_iso8601] $http_host "$request" $status $bytes_sent "$http_referer" "$http_user_agent" $rest_value'
-]
-list_regex = []
-
-for conf in list_cofig:
-    regex = ''.join(
+conf = '$remote_addr $http_x_forwarded_for [$time_iso8601] $http_host "$request" $status $bytes_sent "$http_referer" "$http_user_agent" $rest_value'
+regex = ''.join(
         '(?P<' + g + '>.*)' if g else re.escape(c)
         for g, c in re.findall(r'\$(\w+)|(.)', conf))
-    list_regex.append(regex)
-
-def load_files(log_dir):
-    onlyfiles = [f for f in listdir(log_dir) if isfile(join(log_dir, f))]
-    print ('Số file:', len(onlyfiles))
-    return onlyfiles
 
 def parse_log_to_dict(raw_log = None):
-    m = re.match(list_regex[0], raw_log)
+    m = re.match(regex, raw_log)
     if m:
         return m.groupdict()
     return {}
-    # for regex in list_regex:
-    #     m = re.match(regex, raw_log)
-    #     if m:
-    #         return m.groupdict()
-    # return None
 
-def stream(raw_log, file_name):
-    dict_log = parse_log_to_dict(raw_log)
-    dict_log['file_name'] = file_name
-    # if dict_log:
-    #     future = json_producer.send(TOPIC, dict_log)
-    #     json_producer.flush(30)
-@manager.command
-def run():
-    start = time.time()
-    log_count = 0
-    files = load_files(log_dir)
-    print("===", files)
+def do_work(in_queue, out_list):
+    while True:
+        item = in_queue.get()
+        line_no, line = item
 
-    for file_name in files:
-        with open('{log_dir}/{file_name}'.format(log_dir = log_dir, file_name = file_name)) as logs:
-            for line in logs:
-                log_count += 1
-                # print("===log_count===", log_count)
-                stream(line, file_name)
-    stop = time.time()
+        # exit signal 
+        if line == None:
+            return
 
-    print("===duration===", stop - start)
-if __name__ == '__main__':
-    manager.main()
+        # fake work
+        # time.sleep(.5)
+        result = (line_no, line)
+
+        out_list.append(result)
+
+
+if __name__ == "__main__":
+    num_workers = 4
+
+    manager = Manager()
+    results = manager.list()
+    work = manager.Queue(num_workers)
+
+    # start for workers    
+    pool = []
+    for i in range(0, num_workers):
+        p = Process(target=do_work, args=(work, results))
+        p.start()
+        pool.append(p)
+
+    # produce data
+    with open("/home/vunm/analysis-log/msoha-27-rq.log") as f:
+        iters = itertools.chain(f, (None,)*num_workers)
+        for num_and_line in enumerate(iters):
+            work.put(num_and_line)
+
+    for p in pool:
+        p.join()
+
+    # get the results
+    # example:  [(1, "foo"), (10, "bar"), (0, "start")]
+    print(sorted(results))
